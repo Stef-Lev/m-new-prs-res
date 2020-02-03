066446878b37a2b0da7e714e7cf3ad1bd516c6ee
Stefanos Leventis <54413716+Stef-Lev@users.noreply.github.com>
2020-02-03 17:32:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+//Scrolldown effect
+
+//->Hero scrolldown
+
+$(function () {
+    $('#more').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#navigator").offset().top
+        }, 800);
+    });
+});
+
+//->About scrolldown
+$(function () {
+    $('#aboutBtn').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#work-exp").offset().top
+        }, 800);
+    });
+});
+
+
+//Arrow button animation
+
+$(function () {
+    setTimeout('topArrow()');
+});
+
+function topArrow() {
+    $('#more').animate({
+        marginBottom: '-=10px'
+    }, 800).animate({
+        marginBottom: '+=10px'
+    }, 800);
+    setTimeout('topArrow()', 1600);
+}
+
+$(".meter > span").each(function () {
+    $(this)
+        .data("origWidth", $(this).width())
+        .width(0)
+        .animate({
+            width: $(this).data("origWidth") // or + "%" if fluid
+        }, 1200);
+});
